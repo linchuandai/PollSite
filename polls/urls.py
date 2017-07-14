8ebd6b6679acb2768e4b3693520cbdeb5bd4d199
Leon Dai <linchuandai@gmail.com>
2017-07-13 21:11:09 -0400

    added views and testcases

--- polls/urls.py
+++ polls/urls.py
@@ -5,10 +5,12 @@ from . import view
 
 app_name = 'polls'
 
+#pk = Primary Key
+
 urlpatterns = [
-    url(r'^$', view.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/$', view.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', view.results, name='results'),
+    url(r'^$', view.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', view.DetailView.as_view, name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', view.DetailView.as_view, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', view.vote, name='vote'),
 
 ]
